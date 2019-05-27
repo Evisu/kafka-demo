4f3ea649b366f1e86637fc4378ed48be9b4ebd61
guochen <guochen@zhuoyuan-info.com>
2019-05-27 09:14:35 +0800

    添加了logger

--- src/main/java/com/zy/kafka/demo/KafkaProducer.java
+++ src/main/java/com/zy/kafka/demo/KafkaProducer.java
@@ -5,6 +5,8 @@ import java.util.UUID;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.support.SendResult;
@@ -23,6 +25,8 @@ public class KafkaProducer {
 
     @Autowired
     private KafkaTemplate<String, String> kafkaTemplate;
+    
+    protected final Logger logger = LoggerFactory.getLogger(this.getClass());
 
     /**
      * 定时任务
@@ -31,7 +35,7 @@ public class KafkaProducer {
     public void send(HttpServletRequest request, HttpServletResponse response){
         final String message = UUID.randomUUID().toString();
         ListenableFuture<SendResult<String, String>> future = kafkaTemplate.send("test","标识",message);
-        future.addCallback(o -> System.out.println("send-消息发送成功：" + message), throwable -> System.out.println("消息发送失败：" + message));
+        future.addCallback(o -> logger.info("send-消息发送成功：" + message), throwable -> logger.info("消息发送失败：" + message));
     }
 
 }
